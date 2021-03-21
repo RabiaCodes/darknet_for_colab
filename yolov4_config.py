f6dcc30e01abb972bc513547c5282a91cdeea0db
RabiaCodes <67385673+RabiaCodes@users.noreply.github.com>
2021-03-21 21:03:52 +0900

    Update yolov4_config.py

--- yolov4_config.py
+++ yolov4_config.py
@@ -11,15 +11,15 @@ and cfg/yolov4_custom_test.cfg
 
 """
 
-classes=4
+classes=6
 max_batches=8000
 batch=64
 subdivisions=16
-width=416
-height=416
-channels=3
+width=224
+height=224
+channels=2
 momentum=0.949
 decay=0.0005
-learning_rate=0.001
+learning_rate=0.0005
 steps= (6400,7200)
 scales=(0.1,0.1)
